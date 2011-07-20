f0ff64cb20c28e0a5a3543cf14bea704b32fa389
hadley <h.wickham@gmail.com>
2011-07-20 09:40:19 -0500

    Clean up code

---
+++ R/build.r
@@ -0,0 +1,67 @@
+titles <- c("Article" = "Journal articles", "Book" = "Books", "InCollection" = "Book chapters", "PhdThesis" = "Theses", "TechReport" = "Technical reports", 
+"Unpublished" = "Selected presentations and posters")
+
+paths <- c("Article" = "articles", "Book" = "books", "InCollection" = "chapters", "PhdThesis" = "theses", "TechReport" = "tech-reports", 
+"Unpublished" = "presentations")
+
+make_vita <- function(path = "~/documents/vita/public", templates = "~/documents/vita/vita") {
+  old <- setwd(path)
+  on.exit(setwd(old))
+  
+  me <- read.bib("me.bib", "UTF-8")
+  clean_bib <- function(entry) {
+    entry$title <- str_replace_all(entry$title, "[{}]", "")
+    entry
+  }
+  me <- lapply(me, clean_bib)
+
+  type <- vapply(me, function(x) attr(unclass(x)[[1]], "bibtype"), 
+    FUN.VALUE = character(1))
+  by_type <- split(me, type)
+
+  make_thumbs()
+
+  for(id in seq_along(me)) {
+    entry <- me[[id]]
+    if (str_detect(entry$key, ":")) next()
+
+    page <- render_entry_page(entry)
+
+    values <- new.env(parent = globalenv())
+    values$title <- entry$title
+    values$content <- page$content
+    values$sidebar <- "&nbsp;"
+    values$header <-  page$header
+
+    brew(file.path(templates, "template-article.html"), 
+      str_c("papers/", entry$key, ".html"), envir = values) 
+  }
+
+  for(type in names(titles)) {
+    page <- render_page(by_type[[type]], type)
+
+    values <- new.env(parent = globalenv())
+    values$title <- str_c(titles[type], " by Hadley Wickham.")
+    values$content <- page$content
+    values$sidebar <- page$sidebar
+    values$header <-  page$header
+
+    brew(file.path(templates, "template.html"), 
+      str_c(paths[type], ".html"), envir = values)
+  }
+  
+}
+
+make_thumbs <- function() {
+  papers <- file_path_sans_ext(dir("papers", "*.pdf"))
+  thumbs <- file_path_sans_ext(dir("thumbs"))
+  
+  needed <- setdiff(papers, thumbs)
+  if (length(needed) == 0) return()
+  
+  cmd <- str_c("convert papers/", needed, ".pdf[0] -antialias -thumbnail '100' ",
+    " thumbs/", needed, ".png")
+  lapply(cmd, system)
+  
+  invisible()
+}
---
+++ TODO.md
@@ -0,0 +1,21 @@
+Next up:
+
+* home page, selected bibtex tag
+* atom feed (manual recent selection)
+* check authors ( Publishing Animations, 3D Visualizations, and Movies in JCGS.)
+* sort within a year
+
+* fill in more bibtex
+  * look for DOI / publisher url
+  * add preprint
+  * add abstract to bibtex
+  * check bibtex
+  
+* add recent presentations.
+* all files for 2011
+
+* update cv
+
+* r packages page
+
+* tags so i can easily link to all ggplot2/plyr etc paper/presentations
